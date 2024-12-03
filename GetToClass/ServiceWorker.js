8b00ecfe0702d83312367130e7f168661ae0fd43
cmd644 <cmd644@nau.edu>
2024-12-03 09:55:41 -0700

    Readd the GetToClass build.

---
+++ GetToClass/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Group4G2C-GetToClassFinal-0.1";
+const contentToCache = [
+    "Build/Build12-03-24.loader.js",
+    "Build/Build12-03-24.framework.js",
+    "Build/Build12-03-24.data",
+    "Build/Build12-03-24.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
